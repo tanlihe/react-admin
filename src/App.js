799ee28606dc8a902c39c89a35db0398be72a620
tanlihe <tlh19806704167@163.com>
2021-07-31 19:26:27 +0800

    构建错误页面

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@
  * @Author: Tang
  * @Date: 2021-07-31 16:53:07
  * @LastEditors: Tang
- * @LastEditTime: 2021-07-31 19:06:17
+ * @LastEditTime: 2021-07-31 19:24:05
  */
 // import BaseLayout from "@/layout/base/base";
-import LoginLayout from "./layout/login/login";
+// import LoginLayout from "@/layout/login/login";
+import NotFoundLayout from "@/layout/notFound/notFound";
 function App() {
   // return <BaseLayout />;
-  return <LoginLayout />;
+  return <NotFoundLayout />;
 }
 
 export default App;
---
+++ src/layout/notFound/notFound.js
@@ -0,0 +1,22 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Tang
+ * @Date: 2021-07-31 19:23:00
+ * @LastEditors: Tang
+ * @LastEditTime: 2021-07-31 19:26:01
+ */
+import React, { Component } from "react";
+
+require("./notFound.less");
+
+class NotFoundLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div id="not-found-layout">NotFound</div>;
+  }
+}
+export default NotFoundLayout;
