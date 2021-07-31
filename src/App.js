1ecf859825e0a63459561b75fb02f8dab208bee3
tanlihe <tlh19806704167@163.com>
2021-07-31 21:06:57 +0800

    构建退出页面

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@
  * @Author: Tang
  * @Date: 2021-07-31 16:53:07
  * @LastEditors: Tang
- * @LastEditTime: 2021-07-31 19:24:05
+ * @LastEditTime: 2021-07-31 21:06:28
  */
 // import BaseLayout from "@/layout/base/base";
 // import LoginLayout from "@/layout/login/login";
-import NotFoundLayout from "@/layout/notFound/notFound";
+import LogoutLayout from "@/layout/logout/logout";
+// import NotFoundLayout from "@/layout/notFound/notFound";
 function App() {
-  // return <BaseLayout />;
-  return <NotFoundLayout />;
+  return <LogoutLayout />;
 }
 
 export default App;
---
+++ src/layout/logout/logout.js
@@ -0,0 +1,20 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Tang
+ * @Date: 2021-07-31 21:05:28
+ * @LastEditors: Tang
+ * @LastEditTime: 2021-07-31 21:05:51
+ */
+import React, { Component } from "react";
+
+class LogoutLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h3>LogoutLayout</h3>;
+  }
+}
+export default LogoutLayout;
